ee8100e791706b386c1ed06ce9e49331882764c3
Kevin Barabash <kevinb@khanacademy.org>
2019-02-14 22:41:11 -0500

    dedupe code in jsdom-server.js

--- jsdom-server.js
+++ jsdom-server.js
@@ -6,49 +6,41 @@ const stats = require("./dist/react-loadable.json");
 
 const app = express();
 
+const template = (paths) => `
+<!doctype html>
+<html lang="en">
+    <body>
+        <div id="app"></div>
+        ${paths.map(path => `<script src="/dist/${path}"></script>`).join("\n")}
+    </body>
+</html>
+`;
+
 app.get('/', (req, res) => {
-    const dom = new JSDOM(`
-    <!doctype html>
-    <html lang="en">
-        <body>
-            <div id="app"></div>
-            <script src="/dist/vendors~entry.bundle.js"></script>
-            <script src="/dist/entry.bundle.js"></script>
-        </body>
-    </html>
-    `, {
-        url: "http://localhost:3000/",
-        runScripts: "dangerously",
-        resources: "usable",
-        beforeParse: (window) => {
-            window.onModulesLoaded = (modules) => {
-                const bundles = getBundles(stats, modules)
-                    .filter(bundle => !bundle.file.endsWith(".map"));
+    const entries = ["vendors~entry.bundle.js", "entry.bundle.js"];
 
-                res.send(`
-                <!doctype html>
-                <html lang="en">
-                    <body>
-                        <div id="app"></div>
-                        ${bundles.map(bundle => {
-                            return `<script src="/dist/${bundle.file}"></script>`
-                            // alternatively if you are using publicPath option in webpack config
-                            // you can use the publicPath value from bundle, e.g:
-                            // return `<script src="${bundle.publicPath}"></script>`
-                        }).join('\n')}
-                        <script src="/dist/vendors~entry.bundle.js"></script>
-                        <script src="/dist/entry.bundle.js"></script>
-                    </body>
-                </html>
-                `);
+    const dom = new JSDOM(
+        template(entries),
+        {
+            url: "http://localhost:3000/",
+            runScripts: "dangerously",
+            resources: "usable",
+            beforeParse: (window) => {
+                window.onModulesLoaded = (modules) => {
+                    const bundles = getBundles(stats, modules)
+                        .filter(bundle => !bundle.file.endsWith(".map"))
+                        .map(bundle => bundle.file);
 
-                // The following also works, but the aysnc bundles end up loaded
-                // in <head> instead of in the the <body> where the entry point 
-                // bundles are.
-                // res.send(dom.serialize());
-            };
+                    res.send(template([...bundles, ...entries]));
+
+                    // The following also works, but the aysnc bundles end up loaded
+                    // in <head> instead of in the the <body> where the entry point 
+                    // bundles are.
+                    // res.send(dom.serialize());
+                };
+            },
         },
-    });
+    );
 });
 
 app.use("/dist", express.static("dist"));
